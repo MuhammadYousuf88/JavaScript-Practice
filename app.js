abcc8da3d31336d769d5ef97b900953c42642104
Muhammad Yousuf <152775949+MuhammadYousuf88@users.noreply.github.com>
2024-12-24 23:13:34 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
 
 /* chap 1 
+
+
 alert("Welcome to Website");
+
+alert("Welcome to our Website!");
+
+
+Swal.fire({
+  title: "Good job Yousuf!",
+  text: "You clicked the button!",
+  icon: "success"
+});
+
+Swal.fire({
+  title: "Drag me!",
+  icon: "success",
+  draggable: true
+});
+
+
+
+Qual.sw("Nice to meet you");
+
+Qual.successd("Success","File Uploaded Successfully");
+
+Qual.icondb('Hello Friends','Are you doing something great in life.','')
+
+
+Qual.swh("Welcome","Nice to meet you");
+
+
 */
 
-alert("You clicked the button!");
+
+
+Qual.confirmd("Are you sure you want to continue ", "Click Ok button to continue and Cancel to Close",succ,"OK", "Cancel", "Ok_btn_function","Cancel_btn_function","text","Enter your name here");
+
+
+
