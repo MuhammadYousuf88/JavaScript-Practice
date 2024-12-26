3591a88d3b8518c0a9a3748023b40b9a094ae71e
Muhammad Yousuf <152775949+MuhammadYousuf88@users.noreply.github.com>
2024-12-25 22:46:13 -0800

    Update app.js

--- app.js
+++ app.js
@@ -31,11 +31,60 @@ Qual.icondb('Hello Friends','Are you doing something great in life.','')
 Qual.swh("Welcome","Nice to meet you");
 
 
-*/
 
 
 
 Qual.confirmd("Are you sure you want to continue ", "Click Ok button to continue and Cancel to Close",succ,"OK", "Cancel", "Ok_btn_function","Cancel_btn_function","text","Enter your name here");
+
+
+*/
+
+/*
+
+var firstName = "Muhammad";
+
+var lastName = "Yousuf";
+
+var fullName = firstName + " " + lastName;
+
+console.log(fullName) */
+
+/*
+var fullName = "Viki";
+
+fullName = "Muhammad Yousuf";
+
+console.log("fullName--->",fullName)
+*/
+/*
+Qual.swh(fullName,"Welcome");
+*/
+
+
+/*
+var firstNum = 20;
+var lastNum = 10;
+
+console.log(firstNum+ lastNum)
+
+*/
+
+
+/*
+console.log(10 + 40)
+*/
+
+/*
+var firstValue = "Hello"
+var lastValue = "World"
+
+console.log(firstValue,lastValue,"This is Muhammad Yousuf")
+*/
+
+/*
+console.log(10 % 2)
+*/
+
 
 
 
